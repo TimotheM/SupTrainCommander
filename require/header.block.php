b4d49d9eef29fe53e32f7fa341c7d6ee8a49b8f9
Mauzet Timothé <timothe.mauzet@supinfo.com>
2016-04-20 19:55:44 +0200

    Edit Header

--- require/header.block.php
+++ require/header.block.php
@@ -1,18 +1,24 @@
 <header>
     <div class="row">
+        <?php
+            /* **********************
+               * MENU
+               ********************** */
+        ?>
         <div class="small-12 medium-12 large-12 columns">
             <div class="top-bar">
                 <div class="top-bar-title">
-    									<span data-responsive-toggle="responsive-menu" data-hide-for="medium">
-      										<button class="menu-icon white" type="button" data-toggle></button>
-    									</span>
+                    <span data-responsive-toggle="responsive-menu" data-hide-for="medium">
+                        <button class="menu-icon white" type="button" data-toggle></button>
+                    </span>
                 </div>
                 <div id="responsive-menu">
                     <div class="top-bar-left">
                         <ul class="menu">
-                            <li><a href="#">One</a></li>
-                            <li><a href="#">Two</a></li>
-                            <li><a href="#">Three</a></li>
+                            <li><a href="home">Home</a></li>
+                            <li><a href="login">Login</a></li>
+                            <li><a href="register">Register</a></li>
+                            <li><a href="contact">Contact</a></li>
                         </ul>
                     </div>
                 </div>
@@ -20,3 +26,15 @@
         </div>
     </div>
 </header>
+<section id="submenu">
+    <div class="row">
+        <?php
+            /* **********************
+               * IMG SUB MENU
+               ********************** */
+        ?>
+        <div class="small-12 menu-12 large-12 columns">
+            <img src="img/logo.png" alt="Logo SupTrainCommander">
+        </div>
+    </div>
+</section>

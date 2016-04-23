0b641599c59dbbbc6e1e63324967f20e6b3827e4
Mauzet Timothé <timothe.mauzet@supinfo.com>
2016-04-23 16:44:06 +0200

    Edit footer block & css

--- require/footer.block.php
+++ require/footer.block.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="row">
         <div class="small-12 medium-12 large-12 columns">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, error?</p>
+            <p>Copyright &copy; 2016 Timoth&eacute; M. - All rights reserved.</p>
         </div>
     </div>
 </footer>
